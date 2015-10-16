dcd4d701b55dfd63a2d2e1e74e7a94d504c1501b
miya0001 <miya@wpist.me>
2015-10-17 01:57:57 +0900

    change document root

--- wp-instant-setup.sh
+++ wp-instant-setup.sh
@@ -4,8 +4,9 @@ set -ex;
 
 PORT=${1-8080}
 DB_NAME=${2-wpdev}
+WP_PATH=$(pwd)/public_html
 
-if [ -e "wp-config.php" ]; then
+if [ -e "$$WP_PATH/wp-config.php" ]; then
     php -S 127.0.0.1:$PORT
     exit 0
 fi
@@ -17,16 +18,16 @@ echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u root
 echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u root
 
 echo "WordPressをダウンロードします。"
-wp core download --path=$(pwd) --locale=ja --force
+wp core download --path=$WP_PATH  --locale=ja --force
 
 echo "wp-config.phpを作成します。"
-wp core config --dbhost=localhost --dbname=$DB_NAME --dbuser=root --dbprefix=wp_ --locale=ja --extra-php <<PHP
+wp --path=$WP_PATH core config --dbhost=localhost --dbname=$DB_NAME --dbuser=root --dbprefix=wp_ --locale=ja --extra-php <<PHP
 define( 'JETPACK_DEV_DEBUG', true );
 define( 'WP_DEBUG', true );
 PHP
 
 echo "WordPressをセットアップします。"
-wp core install \
+wp --path=$WP_PATH core install \
 --url=http://127.0.0.1:$PORT \
 --title="$WP_TITLE" \
 --admin_user="admin" \
@@ -34,13 +35,13 @@ wp core install \
 --admin_email="admin@example.com"
 
 echo "パーマリンク設定を変更します。"
-wp rewrite structure "/archives/%post_id%"
+wp --path=$WP_PATH rewrite structure "/archives/%post_id%"
 
 echo "オプションを設定します。"
-wp option update blogdescription "$WP_DESC"
+wp --path=$WP_PATH option update blogdescription "$WP_DESC"
 
 echo "プラグインをインストールします。"
-wp plugin install wp-multibyte-patch --activate
-wp plugin install content-template-engine --activate
+wp --path=$WP_PATH plugin install wp-multibyte-patch --activate
+wp --path=$WP_PATH plugin install content-template-engine --activate
 
-php -S 127.0.0.1:$PORT
+php -S 127.0.0.1:$PORT -t $WP_PATH
