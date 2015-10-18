b3402b726f1a6c1359d5632fae097939fae2f695
miya0001 <miya@wpist.me>
2015-10-18 17:45:22 +0900

    some fix

--- wp-instant-setup.sh
+++ wp-instant-setup.sh
@@ -3,43 +3,71 @@
 set -ex;
 
 DB_NAME=${1-wpdev}
+DB_USER=${2-root}
+DB_PASS=$3
 PORT=8080
 WP_PATH=$(pwd)/www
+WP_TITLE="Welcome to the WordPress"
+WP_DESC="Hello World!"
 
 if [ -e "$WP_PATH/wp-config.php" ]; then
     php -S 127.0.0.1:$PORT -t $WP_PATH
     exit 0
 fi
 
+wp() {
+    bin/wp $*
+}
+
 echo "Download wp-cli"
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
+mkdir bin
+mv wp-cli.phar bin/wp
+chmod 755 bin/wp
 
 echo "path: $WP_PATH" > $(pwd)/wp-cli.yml
-
-WP_TITLE="Welcome to the WordPress"
-WP_DESC="Hello World!"
 
 echo "DROP DATABASE IF EXISTS $DB_NAME;" | mysql -u root
 echo "CREATE DATABASE $DB_NAME DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;" | mysql -u root
 
-php wp-cli.phar core download --path=$WP_PATH --locale=en_US --force
+wp core download --path=$WP_PATH --locale=en_US --force
 
-php wp-cli.phar core config --dbhost=localhost --dbname=$DB_NAME --dbuser=root --dbprefix=wp_ --locale=en_US --extra-php <<PHP
+if [ $DB_PASS ]; then
+wp core config \
+--dbhost=localhost \
+--dbname=$DB_NAME \
+--dbuser=$(printf "%s" $DB_USER) \
+--dbpass=$(printf "%s" $DB_PASS) \
+--dbprefix=wp_ \
+--locale=en_US \
+--extra-php <<PHP
 define( 'JETPACK_DEV_DEBUG', true );
 define( 'WP_DEBUG', true );
 PHP
+else
+wp core config \
+--dbhost=localhost \
+--dbname=$DB_NAME \
+--dbuser=$(printf "%s" $DB_USER) \
+--dbprefix=wp_ \
+--locale=en_US \
+--extra-php <<PHP
+define( 'JETPACK_DEV_DEBUG', true );
+define( 'WP_DEBUG', true );
+PHP
+fi
 
-php wp-cli.phar core install \
+wp core install \
 --url=http://127.0.0.1:$PORT \
---title="$WP_TITLE" \
+--title=$(printf "%s" $WP_TITLE) \
 --admin_user="admin" \
 --admin_password="admin" \
 --admin_email="admin@example.com"
 
-php wp-cli.phar rewrite structure "/archives/%post_id%"
+wp rewrite structure "/archives/%post_id%"
 
-php wp-cli.phar option update blogdescription "$WP_DESC"
+wp option update blogdescription $(printf "%s" $WP_DESC)
 
-php wp-cli.phar theme install twentyfifteen --activate
+wp theme install twentyfifteen --activate
 
 php -S 127.0.0.1:$PORT -t $WP_PATH
